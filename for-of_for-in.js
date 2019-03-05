7e202957fcff8710645137fc89883e3df317b986
jay kim <useless.idler@gmail.com>
2019-03-05 21:29:38 +0900

    for of vs. for in -- modification during iteration

---
+++ for-of_for-in.js
@@ -0,0 +1,20 @@
+(function for_of() {
+  let a = [1,2,3];
+  for (let x of a) {
+    out(x); //-> 1 2 4 3
+    if (x == 2) {
+      a.splice(x,0,4);
+    }
+  }
+})();
+
+(function for_in() {
+  let a = [1,2,3];
+  for (let i in a) {
+    let x = a[i];
+    out(x); //-> 1 2 4
+    if (x == 2) {
+      a.splice(x,0,4);
+    }
+  }
+})();
